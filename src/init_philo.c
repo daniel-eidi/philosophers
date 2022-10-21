7392877d108468d48ad8fb3b2da70a53ce78184d
root <root@DESKTOP-DLGS2TE.localdomain>
2022-10-21 11:03:23 -0300

    correçao de alocacao de memoria

--- src/init_philo.c
+++ src/init_philo.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:45:40 by daeidi-h          #+#    #+#             */
-/*   Updated: 2022/10/20 18:57:32 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2022/10/21 10:21:28 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,27 @@ t_ph_status	**init_ph_stats(char **argv, long init)
 	pthread_mutex_t **forks;
 	int n_philo;
 
-	i = 0;
 	n_philo = ft_atoi(argv[1]);
 	print_lock = malloc (sizeof(pthread_mutex_t));
 	pthread_mutex_init(print_lock, NULL);
 	forks = init_forks (n_philo);
-	ph_stats = calloc(sizeof(t_ph_status *), 1);
+	ph_stats = calloc(sizeof(t_ph_status *), n_philo + 1);
+	i = 0;
 	while (i < n_philo)
 	{
 		ph_stats[i] = calloc(sizeof(t_ph_status), 1);
 		ph_stats[i]->init = init;
-		ph_stats[i]->lst_philos_meal = init;
+		ph_stats[i]->lst_philos_meal = 0;
 		ph_stats[i]->left_fork = forks[i];
 		if (i + 1 == n_philo)
 			ph_stats[i]->right_fork = forks[0];
 		else
 			ph_stats[i]->right_fork = forks[i + 1];
-		ph_stats[i]->id = i + 1;
+		ph_stats[i]->id = (i + 1);
 		ph_stats[i]->print_lock = print_lock;
 		ph_stats[i]->total_ph = n_philo;
 		ph_stats[i]->t_die = ft_atoi(argv[2]) * 1000;
+		printf("ph_stats[%d]->id = %d\n", i, ph_stats[i]->id);
 		ph_stats[i]->t_eat = ft_atoi(argv[3]) * 1000;
 		ph_stats[i]->t_sleep = ft_atoi(argv[4]) * 1000;
 		if (argv[5])
@@ -65,6 +66,6 @@ t_ph_status	**init_ph_stats(char **argv, long init)
 		ph_stats[i]->n_eat = 0;
 		i++;
 	}
-	ph_stats[i] = calloc(sizeof(t_ph_status), 1);
+	ph_stats[i] = NULL;
 	return(ph_stats);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: daeidi-h <daeidi-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 16:55:24 by daeidi-h          #+#    #+#             */
-/*   Updated: 2022/10/20 19:28:17 by daeidi-h         ###   ########.fr       */
+/*   Updated: 2022/10/21 10:40:09 by daeidi-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,16 @@ long init_time (void)
 }
 int	main(int argc, char **argv)
 {
-	int long init;
+	long init;
 	t_ph_status	**ph_stats;
 	int i;
 
 	(void)argc;
 	init = init_time();
 	ph_stats = init_ph_stats(argv, init);
+	i = -1;
+	while (++i < ph_stats[0]->total_ph)
+		printf("ph_stats[%d]->id = %d\n", i, ph_stats[i]->t_die);
 	i = -1;
 	while (++i < ph_stats[0]->total_ph)
 		pthread_create(&ph_stats[i]->pthread_ph, NULL, &philo_routine, ph_stats[i]);
@@ -64,8 +67,12 @@ void*	philo_routine(void *args)
 			pthread_mutex_lock(ph_stats->print_lock);
 			print_actual_time(init, ph_stats->print_lock);
 			dif = current_time(init) - ph_stats->lst_philos_meal;
-			printf("philo %d pegou segundo garfo  e iniciou a comer após %ld ms de jejum\n", ph_stats->id, dif);
+			if (dif > (ph_stats->t_die/1000))
+				printf("philo %d com %ld de jejum morreuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n\n\n\n\n", ph_stats->id, dif);
+			else
+				printf("philo %d pegou segundo garfo  e iniciou a comer após %ld ms de jejum\n", ph_stats->id, dif);			
 			pthread_mutex_unlock(ph_stats->print_lock);
+			ph_stats->lst_philos_meal = current_time(init);
 			usleep(ph_stats->t_eat);
 			pthread_mutex_unlock(ph_stats->right_fork);
 			pthread_mutex_unlock(ph_stats->left_fork);
@@ -83,11 +90,11 @@ void*	philo_routine(void *args)
 			dif = current_time(init) - ph_stats->lst_philos_meal;
 			printf("philo %d pegou segundo garfo  e iniciou a comer após %ld ms de jejum\n", ph_stats->id, dif);
 			pthread_mutex_unlock(ph_stats->print_lock);
+			ph_stats->lst_philos_meal = current_time(init);
 			usleep(ph_stats->t_eat);
 			pthread_mutex_unlock(ph_stats->left_fork);
 			pthread_mutex_unlock(ph_stats->right_fork);
 		}
-		ph_stats->lst_philos_meal = current_time(init);
 		ph_stats->n_eat++;
 		pthread_mutex_lock(ph_stats->print_lock);
 		print_actual_time(init, ph_stats->print_lock);
